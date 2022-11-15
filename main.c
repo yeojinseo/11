917f45da36551056e29880cfecb638532366d292
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-11-15 16:30:41 +0900

    02

--- main.c
+++ main.c
@@ -4,20 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i = 10;
-	char c = 'a';
+	int i = 300;
 	
-	int *iptr;
-	char *cptr;
-	int *iptr2;
+	int *pi = &i;
+	char *pc = &i;
 	
-	iptr = &i;
-	cptr = &c;
-	iptr2 = iptr;
-	
-	printf("i : %p\n%p(size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c : %p\n%p(size:%i)\n", cptr, &c, sizeof(cptr));
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
+	printf("%i, %i, %i\n", i, *pi, *pc);
 	
 	return 0;
 }
