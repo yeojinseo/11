32bacae6c1431492439e749a64431a17c739f891
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-11-15 17:08:14 +0900

    06

--- main.c
+++ main.c
@@ -4,20 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char *pc;    //1byte
-	int *pi;     //4byte
-	double *pd;  //8byte
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double*)10000;
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 	
-	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	**q = 300;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 	
 	return 0;
 }
